149ec2d2d0b02c03fadd590361402d686c3446de
Pankaj Dahiya <pankaj.id.008@gmail.com>
2021-05-08 19:46:57 +0530

    spring initialize java 8 project

---
+++ src/test/java/com/sapient/healthyreps/HealthyrepsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sapient.healthyreps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthyrepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
