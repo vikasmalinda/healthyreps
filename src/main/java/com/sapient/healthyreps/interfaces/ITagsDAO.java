57026e44c41cb0153ae96574c01c3ab3fad52e77
19sanchit19 <sanchitcn@gmail.com>
2021-05-14 03:53:06 +0530

    Tags entity, DAO, and interface added.

---
+++ src/main/java/com/sapient/healthyreps/interfaces/ITagsDAO.java
@@ -0,0 +1,26 @@
+package com.sapient.healthyreps.interfaces;
+
+import java.util.List;
+
+import com.sapient.healthyreps.entity.Tags;
+
+public interface ITagsDAO {
+
+	
+	public boolean insertTag(int pid, String content);
+	
+	public List<Tags> getAllTagsByPostId(int pid);
+	
+	public List<Tags> getAllTags();
+	
+	public boolean deleteTagbyId(int id);
+	
+	public boolean deleteAllTagsByPostId(int pid);
+	
+	public boolean updateTag(int id, String content);
+	
+	
+	
+}
+
+
