e90f6dbec1dc2a8871c83b57d983b27468b237bf
shivi-25 <shivani2512000@gmail.com>
2021-05-16 13:02:01 +0530

    Media Controller

---
+++ src/test/java/com/sapient/healthyreps/MediaDAOTest.java
@@ -0,0 +1,52 @@
+package com.sapient.healthyreps;
+import java.sql.Date;
+import java.util.ArrayList;
+
+import java.sql.Timestamp;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.sapient.healthyreps.dao.MediaDAO;
+import com.sapient.healthyreps.entity.Media;
+
+
+class MediaDAOTest {
+
+	//Comments_postDAO comment_postDao;
+	MediaDAO mediaDao;
+	
+
+	@BeforeEach
+	void initialize() {
+		this.mediaDao = new MediaDAO();
+	}
+
+	@Test
+	void CRUDTestPass() {
+		
+		// Create Test
+
+
+
+		// Read Test
+
+		
+
+		// Update test
+
+		
+
+		// Delete test
+
+		
+
+	}
+
+}
