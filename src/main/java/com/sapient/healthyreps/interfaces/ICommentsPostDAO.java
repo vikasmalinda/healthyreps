3f98879d1dd8e54331610d22255ed7ca541f12a2
speedyshreya <shreya251199@gmail.com>
2021-05-22 00:07:12 +0530

    Update and rename IComments_postDAO.java to ICommentsPostDAO.java

--- src/main/java/com/sapient/healthyreps/interfaces/ICommentsPostDAO.java
+++ src/main/java/com/sapient/healthyreps/interfaces/ICommentsPostDAO.java
@@ -3,25 +3,25 @@ package com.sapient.healthyreps.interfaces;
 import java.sql.Timestamp;
 import java.util.List;
 
-import com.sapient.healthyreps.entity.Comments_post;
+import com.sapient.healthyreps.entity.CommentsPost;
 import com.sapient.healthyreps.exception.InvalidId;
 
 public interface IComments_postDAO {
 
 	public void checkCommentId (int comid) throws InvalidId;
 	public boolean insertComment(int comid, int votes, String content, int pid, int uid, Timestamp timestamp, int reported);
-	public boolean insertComment(Comments_post comment);
+	public boolean insertComment(CommentsPost comment);
 	public boolean updateComment(int comid, int votes, String content, int pid, int uid, Timestamp timestamp, int reported);
-	public boolean updateComment(Comments_post comment);
+	public boolean updateComment(CommentsPost comment);
 	public boolean deleteCommentById(int comid);
-	public List<Comments_post> getAllComments();
+	public List<CommentsPost> getAllComments();
 	
-	public List<Comments_post> getAllCommentsByPostId(int pid);
-	public Comments_post getCommentByCommentId(int comid);
+	public List<CommentsPost> getAllCommentsByPostId(int pid);
+	public CommentsPost getCommentByCommentId(int comid);
 	public boolean updateVotebyCommentId(int comid, int votes);
 	
 	public boolean updateStatusofComment(int comid,int newstatus);
 	
-	public Comments_post getMostVotedComment(int comid);
+	public CommentsPost getMostVotedComment(int comid);
 	
 }
