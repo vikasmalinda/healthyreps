29eb32ec1b0a5b16a8effc70c5a896a83d5bc8ef
Pankaj Dahiya <pankaj.id.008@gmail.com>
2021-05-27 15:40:53 +0530

    solving errors

--- src/main/java/com/sapient/healthyreps/interfaces/IGalleryDAO.java
+++ src/main/java/com/sapient/healthyreps/interfaces/IGalleryDAO.java
@@ -1,5 +1,13 @@
 package com.sapient.healthyreps.interfaces;
 
-public class IGalleryDao {
-    
+import java.util.List;
+
+import com.sapient.healthyreps.entity.Gallery;
+
+public interface IGalleryDAO {
+	public boolean insertNewImage(Gallery user);
+
+	public List<Gallery> getAllImages();
+
+	public List<String> getUserImageLinks(int uid);
 }
