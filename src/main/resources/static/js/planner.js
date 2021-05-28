af5ea9197808def1758a6956decfd4627169205d
Bitan Sarkar <bs29@iitbbs.ac.in>
2021-05-28 11:59:23 +0530

    planner, workout and calculator added

---
+++ src/main/resources/static/js/planner.js
@@ -0,0 +1,7 @@
+
+      function dontShow() {
+        document.getElementById("addData").style.display = "none";
+      }
+      function doShow() {
+        document.getElementById("addData").style.display = "block";
+      }
